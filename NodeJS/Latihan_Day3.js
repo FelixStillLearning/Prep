41db08d5687bee3b325ef10155772bc3d7957b41
FelixStillLearning <felixangga077@gmail.com>
2026-01-26 13:15:51 +0700

    Latihan_Day3.js

---
+++ NodeJS/Latihan_Day3.js
@@ -0,0 +1,16 @@
+// menstruktur data dengan Object 
+const user = {};
+const products = { name: "sepatu", price: 23000};
+
+
+const user1 = {
+    name: 'felix',
+    'lastname' : 'Angga Resky',
+    age : 9
+}
+
+console.log(user1)
+console.log(user1.name)
+console.log(user1.lastname)
+
+const ( name,lastname ) = user3;
